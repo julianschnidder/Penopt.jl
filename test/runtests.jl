52a0079dc7533ae36e63a2668c4b4e0ac4839805
Julian Schnidder <j.schnidder@gmail.com>
2020-11-04 21:00:19 +0100

    adapted test tolerance

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ f(x) = norm(x).^2
 c(x) = x - 20 * ones(length(x))
 @testset "Penopt.jl" begin  
     xopt,fxopt = penopt(f,c,ones(2),[0 1 2 4 8 16 32 64])
-    @test  xopt ≈ [0,0] atol 0.001
-    @test fxopt ≈ 0.0 atol 0.001
+    @test  xopt ≈ [0,0] atol = 0.0001
+    @test fxopt ≈ 0.0   atol = 0.001
 end
