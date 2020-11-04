35f96d6c698f34a643e269f9e05533c574692bae
Julian Schnidder <j.schnidder@gmail.com>
2020-11-04 20:49:25 +0100

    improve test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using Penopt
-using LinearAlgebra
-f(x)=norm(x).^2
-c(x)=x-20*ones(length(x))
-penopt(f,c,ones(2),[0 1 2 4 8 16 32 64])
+using LinearAlgebra: norm
+using Test
+
+f(x) = norm(x).^2
+c(x) = x - 20 * ones(length(x))
+@testset "Penopt.jl" begin  
+    xopt,fxopt = penopt(f,c,ones(2),[0 1 2 4 8 16 32 64])
+    @test  xopt ≈ [0,0] atol 0.001
+    @test fxopt ≈ 0.0 atol 0.001
+end
